b3330f082f116f03a46f848bd150a539e656164a
KajitsakPansit <kajitsak1111@gmail.com>
2024-02-13 13:32:02 +0700

    LabSheet11

--- LabSheet11/src/Director.java
+++ LabSheet11/src/Director.java
@@ -1,63 +1,33 @@
 public class Director {
-
 	private String name;
-
 	private String email;
-
 	private char gender;
-
 	public Director(String name ,String email,char gender) {
-
 		this.name=name;
-
 		this.email=email;
-
 		this.gender=gender;
-
 	}
-
 	public Director(String name,String email) {
-
 		this.name=name;
-
 		this.email=email;
-
 	}
-
 	public String getName() {
-
 		return this.name;
-
 	}
-
 	public String getEmail() {
-
 		return this.email;
-
 	}
-
 	public String getGenderName() {
-
 		if(this.gender=='m'||this.gender=='M') {
-
 			return "Male";
-
 		}
-
 		else if(this.gender=='f'||this.gender=='F') {
-
 			return "Female";
-
 		}
-
 		else
-
 			return "Null";
-
 	}
-
 	public String toString() {
-
 		return this.name+"("+this.email+";"+getGenderName()+")";
 
 	}
